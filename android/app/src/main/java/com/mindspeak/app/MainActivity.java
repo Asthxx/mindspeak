cff347df92b7de5572df181422ee866505714691
Asthxx <qw1346ee@163.com>
2026-08-20 18:48:22 +0800

    feat: register HttpServerPlugin in MainActivity

--- android/app/src/main/java/com/mindspeak/app/MainActivity.java
+++ android/app/src/main/java/com/mindspeak/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.mindspeak.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.mindspeak.app.HttpServerPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(HttpServerPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
